76a6dcf7ddaf8f0a2b165f97f758411d9cd1efb0
dobro12 <rlaehgudhoho@gmail.com>
2023-10-29 20:00:11 +0900

    modify train shell

--- tf1/train_ant.sh
+++ tf1/train_ant.sh
@@ -1,1 +1,4 @@
 python main.py --name TRC-Car --seed 1 --env_name Safexp-CarGoal1-v0 --wandb
+python main.py --name TRC-Car --seed 2 --env_name Safexp-CarGoal1-v0 --wandb
+python main.py --name TRC-Car --seed 3 --env_name Safexp-CarGoal1-v0 --wandb
+python main.py --name TRC-Car --seed 4 --env_name Safexp-CarGoal1-v0 --wandb
