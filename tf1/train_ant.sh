f5016d1c3eb9b878bd6ec2b129e0a33e12074e01
dobro12 <rlaehgudhoho@gmail.com>
2023-10-29 20:00:11 +0900

    modify tf1/train_ant.sh

--- tf1/train_ant.sh
+++ tf1/train_ant.sh
@@ -2,3 +2,5 @@ python main.py --name TRC-Ant --seed 1 --env_name Safexp-AntGoal1-v0 --wandb
 python main.py --name TRC-Ant --seed 2 --env_name Safexp-AntGoal1-v0 --wandb
 python main.py --name TRC-Ant --seed 3 --env_name Safexp-AntGoal1-v0 --wandb
 python main.py --name TRC-Ant --seed 4 --env_name Safexp-AntGoal1-v0 --wandb
+python main.py --name TRC-Ant --seed 5 --env_name Safexp-AntGoal1-v0 --wandb
+python main.py --name TRC-Ant --seed 6 --env_name Safexp-AntGoal1-v0 --wandb
