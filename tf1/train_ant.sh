63cb284690b4ac1c5cdf020e5cb213185ad68c55
dobro12 <rlaehgudhoho@gmail.com>
2023-10-29 20:00:11 +0900

    modify train shell script

--- tf1/train_ant.sh
+++ tf1/train_ant.sh
@@ -1,6 +1,6 @@
-python main.py --name TRC-Ant --seed 1 --env_name Safexp-AntGoal1-v0 --wandb
-python main.py --name TRC-Ant --seed 2 --env_name Safexp-AntGoal1-v0 --wandb
-python main.py --name TRC-Ant --seed 3 --env_name Safexp-AntGoal1-v0 --wandb
-python main.py --name TRC-Ant --seed 4 --env_name Safexp-AntGoal1-v0 --wandb
-python main.py --name TRC-Ant --seed 5 --env_name Safexp-AntGoal1-v0 --wandb
-python main.py --name TRC-Ant --seed 6 --env_name Safexp-AntGoal1-v0 --wandb
+python main.py --gpu_idx 1 --name TRC-Ant --seed 1 --env_name Safexp-AntGoal1-v0 --wandb
+python main.py --gpu_idx 1 --name TRC-Ant --seed 2 --env_name Safexp-AntGoal1-v0 --wandb
+python main.py --gpu_idx 1 --name TRC-Ant --seed 3 --env_name Safexp-AntGoal1-v0 --wandb
+python main.py --gpu_idx 1 --name TRC-Ant --seed 4 --env_name Safexp-AntGoal1-v0 --wandb
+python main.py --gpu_idx 1 --name TRC-Ant --seed 5 --env_name Safexp-AntGoal1-v0 --wandb
+python main.py --gpu_idx 1 --name TRC-Ant --seed 6 --env_name Safexp-AntGoal1-v0 --wandb
